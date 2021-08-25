91d651dc788d113206e539aa532ad594923e9114
mrmchughes <mrmchughes27@gmail.com>
2021-08-25 09:16:52 -0700

    done with all steps, need to add CSS

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //myLibrary stores the books
-let myLibrary = [];
+let myLibrary = [
+   {
+      title: 'Test Title',
+      author: 'Test Author',
+      pages: 'Test Pages',
+   }
+];
+
+let str = localStorage.getItem('myLibrary');
+
+let parsedObj = JSON.parse(str);
 
 //the Book function creates new Book objects
 function Book(title, author, pages, read) {
@@ -37,6 +47,12 @@ function addBookToLibrary() {
          pages,
       );
       myLibrary.push(book);
+
+      let jsonMyLibrary = JSON.stringify(myLibrary);
+
+      localStorage.setItem('myLibrary', jsonMyLibrary);
+
+      
    
       //once new Book is pushed to array, clears the previous values
       document.getElementById('titleField').value = '';
@@ -45,40 +61,56 @@ function addBookToLibrary() {
    };
 };
 
-function removeBookFromLibrary(){
-   //removes selected book from library onclick
-}
-
 //bookDisplay() displays all the books currently in the library array
 function bookDisplay(){
+   let container = document.getElementById('libraryContainer');
+   container.innerHTML = '';
+
    for (let i = 0; i < myLibrary.length; i++) {
       console.log("I am bookDisplay!");
 
+      let bookContainer = document.createElement('div');
       let book = myLibrary[i];
-      let container = document.getElementById('libraryContainer');
 
-      let div = document.createElement('div');
-      div.innerHTML += book.title + ' by ' + book.author + ',' + ' is ' + book.pages + ' pages long' + '.';  
+      let bookDiv = document.createElement('div'); 
 
-      let readStatusButtons = document.getElementsByName('readRadios');
-      
-      for (let i = 0; i < readStatusButtons.length; i++) {
-         if (readStatusButtons[i].checked){
-            div.innerHTML += readStatusButtons[i].value;
+      let bookText = document.createElement('p');
+      bookText.textContent = book.title + ' by ' + book.author + ',' + ' is ' + book.pages + ' pages long' + '.';
 
-            break;
-         }
-      }
+      let haveBeenReadText = document.createElement('p'); 
+
+      //marks a book as read
+      let readBookButton = document.createElement('button');
+      readBookButton.setAttribute('type', 'button');
+      readBookButton.setAttribute('class', 'btn');
+      readBookButton.textContent = 'Click to mark book as read.';
+
+      readBookButton.addEventListener('click', bookRead);
+
+      function bookRead() {   
+         haveBeenReadText.textContent = 'This book has been read.';
+      };
 
       //run removeBookFromLibrary onclick
-      let button = document.createElement('button');
-      button.setAttribute('type', 'button');
-      button.setAttribute('class', 'btn');
-      button.textContent = 'Click here to remove this book.';
+      let removeBookButton = document.createElement('button');
+      removeBookButton.setAttribute('type', 'button');
+      removeBookButton.setAttribute('class', 'btn');
+      removeBookButton.textContent = 'Click here to remove this book.';
 
-      container.appendChild(button);
+      removeBookButton.addEventListener('click', removeBookFromLibrary);
 
-      container.appendChild(div);
+      function removeBookFromLibrary(){
+         //removes selected book from library onclick
+         myLibrary.splice([i], 1)
+      }
+
+      //appends elements to a Book's entry div
+      bookDiv.appendChild(bookText);
+      bookDiv.appendChild(haveBeenReadText);
+      bookContainer.appendChild(readBookButton);
+      bookContainer.appendChild(removeBookButton);
+      bookContainer.appendChild(bookDiv);
+      container.appendChild(bookContainer);
    }
 };
 //test book example below
